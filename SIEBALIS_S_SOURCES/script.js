e14ab0e51a7845d68faa532734543b826868505b
Kultost-hub <kira07533@gmail.com>
2026-05-18 21:51:42 +0300

    Update script.js

--- SIEBALIS_S_SOURCES/script.js
+++ SIEBALIS_S_SOURCES/script.js
@@ -336,7 +336,7 @@ const difficultyBtns = document.querySelectorAll('.difficulty');
 let bombs = [];
 let numbers = [];
 let numberColors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#1abc9c', '#34495e', '#7f8c8d',];
-let endscreenContent = {win: '<span>✔</span> you won!', loose: '💣 Booom! Game over.'};
+let endscreenContent = {win: '<span>Победа</span>', loose: 'Лох'};
 
 let gameOver = false;
 
